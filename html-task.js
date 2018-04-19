9ec3e14350fce41c1b9cc4157a7428e7919c1e6a
FlynnLee <593029372@qq.com>
2018-04-19 09:53:49 +0800

    require task update

--- html-task.js
+++ html-task.js
@@ -69,68 +69,80 @@ const htmlTask = (
         return function amdHtmlPlugin(tree, cb) {
             const tasks = []
 
-            tree.match({ tag: 'script', attrs: { amd: '' } }, node => {
-                const src = node.attrs.src
+            tree.match({ tag: 'script' }, node => {
+                if ('amd' in node.attrs) {
+                    const src = node.attrs.src
 
-                const input = Path.join(rootDir, src)
-                const relativeEntry = getEntry(input, SRC_DIR)
-                const ext = Path.extname(input)
-                const hash = md5(fse.readFileSync(input, { encoding: 'utf-8' }))
+                    const input = Path.join(rootDir, src)
+                    const relativeEntry = getEntry(input, SRC_DIR)
+                    const ext = Path.extname(input)
+                    const hash = md5(
+                        fse.readFileSync(input, { encoding: 'utf-8' })
+                    )
 
-                const file = getDistname({
-                    name: relativeEntry,
-                    hash,
-                    ext: ext,
-                    type: 'js'
-                })
+                    const file = getDistname({
+                        name: relativeEntry,
+                        hash,
+                        ext: ext,
+                        type: 'js'
+                    })
 
-                rollupConfig.inputOptions.input = input
-                rollupConfig.inputOptions.onwarn = function(warning) {
-                    console.warn(warning.message,'->',input)
+                    rollupConfig.inputOptions.input = input
+                    rollupConfig.inputOptions.onwarn = function(warning) {
+                        console.warn(warning.message, '->', input)
+                    }
+                    rollupConfig.outputOptions.file = file
+
+                    const rollupTask = rollup
+                        .rollup(rollupConfig.inputOptions)
+                        .then(bundle =>
+                            bundle.write(rollupConfig.outputOptions)
+                        )
+                        .then(() => {
+                            node.attrs['src'] = requirejs
+                            delete node.attrs.amd
+
+                            const filecontent = fse.readFileSync(file, {
+                                encoding: 'utf-8'
+                            })
+                            let newContent = `
+                                    window.process = window.process || {};
+                                    window.process.env = window.process.env || {};
+                                    window.process.env.NODE_ENV = '${
+                                        process.env.NODE_ENV
+                                    }';`
+                            newContent += `require.config(${JSON.stringify(
+                                requireConfig,
+                                null,
+                                2
+                            )});`
+                            newContent += `
+                                    if(process.env.NODE_ENV==='development'){
+                                        var s= document.createElement('script');
+                                        s.src = 'http://HOST:${3000}/browser-sync/browser-sync-client.js'.replace('HOST',location.hostname);
+                                        document.head.appendChild(s);
+                                    };
+                                `
+                            if(node.attrs.amd){
+                                newContent+=`window.${node.attrs.amd} = function(){`
+                            }
+                            newContent += filecontent
+                            if(node.attrs.amd){
+                                newContent+= '})'
+                            }
+                            node.attrs['data-main'] = getUrl({
+                                name: relativeEntry,
+                                hash,
+                                ext,
+                                type: 'js'
+                            })
+                            return ensureOutputFile(file, newContent)
+                        })
+                        .catch(e => {
+                            console.error(e)
+                        })
+                    tasks.push(rollupTask)
                 }
-                rollupConfig.outputOptions.file = file
-
-                const rollupTask = rollup
-                    .rollup(rollupConfig.inputOptions)
-                    .then(bundle => bundle.write(rollupConfig.outputOptions))
-                    .then(() => {
-                        node.attrs['src'] = requirejs
-                        delete node.attrs.amd
-
-                        const filecontent = fse.readFileSync(file, {
-                            encoding: 'utf-8'
-                        })
-                        let newContent = `
-                                window.process = window.process || {};
-                                window.process.env = window.process.env || {};
-                                window.process.env.NODE_ENV = '${
-                                    process.env.NODE_ENV
-                                }';`
-                        newContent += `require.config(${JSON.stringify(
-                            requireConfig,
-                            null,
-                            2
-                        )});`
-                        newContent += `
-                                if(process.env.NODE_ENV==='development'){
-                                    var s= document.createElement('script');
-                                    s.src = 'http://HOST:${3000}/browser-sync/browser-sync-client.js'.replace('HOST',location.hostname);
-                                    document.head.appendChild(s);
-                                };
-                            `
-                        newContent += filecontent
-                        node.attrs['data-main'] = getUrl({
-                            name: relativeEntry,
-                            hash,
-                            ext,
-                            type: 'js'
-                        })
-                        return ensureOutputFile(file, newContent)
-                    })
-                    .catch(e => {
-                        console.error(e)
-                    })
-                tasks.push(rollupTask)
 
                 return node
             })
